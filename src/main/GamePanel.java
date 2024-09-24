46ce35fd33c01cbe703e9ae9e5ed6275da21c1f0
AndreasOtavio <otaviorosiano@gmail.com>
2024-09-24 11:05:42 -0300

    Creating gameloop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,39 +5,68 @@ import Inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
+    private Color color = new Color(150,20,90);
+    private Random random;
+
 
     public GamePanel(){
-        addKeyListener(new KeyBoardInputs(this));
+        random = new Random();
         mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyBoardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
     public void changeXDelta (int value){
         this.xDelta +=value;
-        repaint();
     }
     public void changeYDelta (int value){
         this.yDelta +=value;
-        repaint();
     }
 
     public void setRectPosition(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.fillRect(xDelta,yDelta,200,50);
+        updateRect();
+        g.setColor(color);
+
+        g.fillRect((int)xDelta,(int)yDelta,200,50);
+
+
     }
+    private void updateRect() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            color = getRandomColor();
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            color = getRandomColor();
+        }
+    }
+
+private Color getRandomColor (){
+    int r = random.nextInt(255);
+    int g = random.nextInt(255);
+    int b = random.nextInt(255);
+
+    return new Color(r, b,b);
+}
 
 
 
